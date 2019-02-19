cb89ab5e40f68baceb4893bb818a25238c2c4866
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-02-19 23:22:18 +0500

    Update argand.py

--- argand.py
+++ argand.py
@@ -13,7 +13,8 @@ Y = array(Y)
 plt.plot(real(Y), imag(Y), 'r')
 plt.ylabel('Imaginary')
 plt.xlabel('Real')
-
+plt.axhline(y=0,color='black')
+plt.axvline(x=0, color='black')
 
 
 def argand(complex_number):
@@ -23,13 +24,12 @@ def argand(complex_number):
     y = complex_number
     x1,y1 = [0,real(y)], [0, imag(y)]
     x2,y2 = [real(y), real(y)], [0, imag(y)]
-    plt.axhline(y=0,color='black')
-    plt.axvline(x=0, color='black')
+    
 
     plt.plot(x1,y1, 'r') # Draw the hypotenuse
     plt.plot(x2,y2, 'r') # Draw the projection on real-axis
 
     plt.plot(real(y), imag(y), 'bo')
 
-[argand(r*exp(1j*theta)) for theta in linspace(0,pi/2,5)]
+[argand(r*exp(1j*theta)) for theta in linspace(0,2*pi,100)]
 plt.show()
